665d47b84836139fcd6be34581f6546cd0e3489e
qqcloud <jeri.shi@qq.com>
2016-07-17 19:57:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-﻿var express = require('express');
+var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var methodOverride = require('method-override');
 var request = require('request');
 var logger = require('morgan');
-var fs = require("fs");
 var proxyConfig = require('./config/proxyConfig');
 var routesConfig = require('./routes/index');
 
 app.use(logger('dev'));
 app.use(require('./middlewares/basicAuth'));
 app.use(methodOverride());
-
 // 请求示例：http://127.0.0.1/part4/upload
 app.use('/', function(req, res) {
+
 	var pathArr = req.path.split('/');
 	// 针对不在配置文件中的路由进行错误处理
-	if (!proxyConfig[pathArr[1]] || !routesConfig[req.path]) {
+	if (!proxyConfig[pathArr[1]]) {
 
 		res.status(404).send('Not found:' + req.originalUrl);
 		return;
 	}
 	// 设置转发url
 	var targetUrl = 'http://' + proxyConfig[pathArr[1]].host + ':' + proxyConfig[pathArr[1]].port + req.originalUrl.replace(/\/part\d/, '');
-	// 重置请求方式
-	req.method = routesConfig[req.path].method;
+	// 重置请求方式，配置表里没有的路由，使用初始method
+	if (routesConfig[req.path]) {
+		// 如果路由没有配置method，则使用初始method
+		req.method = routesConfig[req.path].method || req.method;
+	}
 	// 不能使用bodyParser，会把req里数据流进行更改，对pipe方法造成影响
 	req.pipe(request(targetUrl)).on('error', function(err) {
 		// 处理目标服务器错误
@@ -42,4 +44,4 @@ app.use('/', function(req, res) {
 	}).pipe(res);
 });
 
-server.listen(5000);
+server.listen(3100);
